2d7c00e0513a8bd10fe2e89e840298dbb9deafe8
KAKU-KRCAMP <kaku.krcamp@gmail.com>
2019-09-15 13:24:04 +0900

    パーフェクトPHPのインデックスを作成

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>パーフェクトPHP インデックス</title>
+</head>
+<body>
+    <h1>パーフェクトPHP インデックス</h1>
+    <h2>6章</h2>
+    <p>
+        <a href="./wordboard/bbs.php" target="_blank">ひとこと掲示板</a>
+    </p>
+</body>
+</html>
